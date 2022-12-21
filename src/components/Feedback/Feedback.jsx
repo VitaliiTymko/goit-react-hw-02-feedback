f99ec42ef0062cbab55e793fc4e358c8f2c59184
VitaliiTymko <intehmet2015@gmail.com>
2022-12-22 01:56:12 +0200

    21.12.2022

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -7,20 +7,46 @@ class Feedback extends React.Component {
     bad: 0,
   };
 
+  addsReviewGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  addsReviewNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+
+  addsReviewBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+  // const totalReview = Number{ this.addsReviewGood } + Number{ this.addsReviewNeutral } + Number{ this.addsReviewBad }
+  // console.log(totalReview);
   render() {
     return (
       <div>
         <h1>Please leave feedback</h1>
         <div>
-          <button type="button">Good</button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button>
+          <button type="button" onClick={this.addsReviewGood}>
+            Good
+          </button>
+          <button type="button" onClick={this.addsReviewNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.addsReviewBad}>
+            Bad
+          </button>
         </div>
         <h2>Statistics</h2>
         <div>
-          <p>Good: 2</p>
-          <p>Neutral: 3</p>
-          <p>Bad: 2</p>
+          {this.state.good > 0 && <p>Good: {this.state.good}</p>}
+          {this.state.neutral > 0 && <p>Neutral: {this.state.neutral}</p>}
+          {this.state.bad > 0 && <p>Bad: {this.state.bad}</p>}
+          {/* <p>Total{totalReview }</p> */}
         </div>
       </div>
     );
