7e53e53e9ce8f97ca9cd7891cc7e924041400138
VitaliiTymko <intehmet2015@gmail.com>
2023-01-02 22:35:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,7 @@ import Feedback from './Feedback/Feedback';
 
 export const App = () => {
   return (
-    <div
-      style={
-        {
-          // height: '100vh',
-          // display: 'flex',
-          // justifyContent: 'center',
-          // alignItems: 'center',
-          // fontSize: 40,
-          // color: '#010101',
-        }
-      }
-    >
+    <div>
       <Feedback />
     </div>
   );
